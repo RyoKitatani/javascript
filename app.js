5e91ea592f3ef7d5c005d2289b77b939c6eec323
EC2 Default User <ec2-user@ip-172-31-23-80.ec2.internal>
2021-02-16 22:41:24 +0000

    [Add]じゃんけん関数作成

--- app.js
+++ app.js
@@ -70,8 +70,59 @@ function addString(strA){
 }
 
 var promptStr = prompt('何か好きな文字を入力してください。');
-
 alert(promptStr);
 
 
+var user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください');
+var js_hand = getJhand();
+var judge = winLose(user_hand, js_hand);
+alert('あなたが選んだ手は'+ user_hand + 'です\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は、' + judge + 'です。');
 
+function getJhand (){
+    var js_hand_num = Math.floor( Math.random() * 3);    
+    var hand_name;
+    
+    if (js_hand_num == 0 ){
+        js_hand = "グー";
+    } else if (js_hand_num == 1 ){
+        js_hand = "チョキ";
+    } else if (js_hand_num == 2 ){
+        js_hand = "パー";
+    }
+    
+    return js_hand;
+}
+
+function winLose(user, js){
+    var winLoseStr;
+    
+    if (user == "グー"){
+      if(js =="グー"){
+          winLoseStr = "あいこ";
+      } else if (js =="チョキ"){
+          winLoseStr = "勝ち";
+      } else if (js =="パー"){
+          winLoseStr = "負け";
+      }
+    } else if (user == "チョキ"){
+      if(js =="グー"){
+          winLoseStr = "負け";
+      } else if (js =="チョキ"){
+          winLoseStr = "あいこ";
+      } else if (js =="パー"){
+          winLoseStr = "勝ち";
+      }
+    } else if (user == "パー"){
+      if(js =="グー"){
+          winLoseStr = "勝ち";
+      } else if (js =="チョキ"){
+          winLoseStr = "負け";
+      } else if (js =="パー"){
+          winLoseStr = "あいこ";
+      }
+    }
+    
+    return winLoseStr
+}
+
+ 
