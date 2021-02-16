79900b5fa233303fd6d548c6eead34e55087e008
EC2 Default User <ec2-user@ip-172-31-23-80.ec2.internal>
2021-02-16 14:08:14 +0000

    [Add]整数の変数

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-alert('Hellow World');
+// alert('Hellow World') ;
 
-var hello = "Hello, World!";
-alert(hello);
+var hello = "Hello, World!" ;
+alert(hello) ;
+
+var int1 = 1 ;
+var int2 = -10 ;
+var float1 = 3.14 ;
+var str1 = 'JavaScriptを覚えよう' ;
+
+alert(int1)
