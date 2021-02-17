81664eafa358bd3406304f3a4a279b0ac4d18778
EC2 Default User <ec2-user@ip-172-31-23-80.ec2.internal>
2021-02-17 06:43:21 +0000

    [Add]5章確認問題

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -5,6 +5,26 @@
 //   });
 // });
 
+// $(function(){
+//   $('.box1').slideDown();
+//   $('.box1').slideUp();
+// });
+
+// $(function(){
+//   $('.box1').show();
+//   $('.box1').css({'background-color': '#0000FF'});
+// });
+
+// $(function(){
+//   $('.box1').hide();
+// });
+
 $(function(){
-  $('.box1').slideDown();
+  $('.box1').slideDown(function(){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'height': '100px',
+      'width': '200px'
+    }).slideUp();
+  });
 });
